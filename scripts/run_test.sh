2b841f94e2263f58f853321f0d19c6fab86045f7
Ian O Connell <ioconnell@twitter.com>
2015-05-01 14:03:29 -0700

    updates

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,6 +2,7 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 cd $BASE_DIR
 
+ls -lh /home/travis/.sbt/launchers/0.13.8/sbt-launch.jar
 rm -rf ~/.sbt
 
 export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=128m -Xms256m -Xmx512m -Xss2m"
@@ -18,5 +19,8 @@ bash -c "while true; do echo -n .; sleep 5; done" &
 PROGRESS_REPORTER_PID=$!
 time ./sbt "$(withCmd "compile; test:compile")" &> /dev/null
 kill -9 $PROGRESS_REPORTER_PID
+
+echo "Looking for sbt launch stuff?"
+ls -lh /home/travis/.sbt/launchers/0.13.8/sbt-launch.jar
 
 ./sbt "$(withCmd test)"
